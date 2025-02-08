ce9bc8d52692c397d2406f899361ac581446b760
Davi Silva <davi.pimenta8@gmail.com>
2025-02-08 18:58:56 -0300

    fix errors in server file

--- api/server.js
+++ api/server.js
@@ -1,17 +1,20 @@
 const jsonServer = require("json-server");
+const fs = require("fs");
+const path = require("path");
 
 const server = jsonServer.create();
 
-const router = jsonServer.router("db.json");
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
 
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
   })
 );
 server.use(router);
